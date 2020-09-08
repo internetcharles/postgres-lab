8717ecacfe708a5c57c0ce64451ed7c64fc2cf08
internetcharles <charlesodellsmith@gmail.com>
2020-09-08 16:00:30 -0700

    insert test two

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,16 @@
 DROP TABLE IF EXISTS pizzas;
+DROP TABLE IF EXISTS games;
 
 CREATE TABLE pizzas (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
     toppings INT,
     price INT CHECK (price > 0)
+);
+
+CREATE TABLE games (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    rating INT CHECK (rating > 0)
 );
