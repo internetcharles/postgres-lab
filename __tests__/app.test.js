13fa48c14c7d6a9e348d056dff2aecd82e2d8f07
internetcharles <charlesodellsmith@gmail.com>
2020-09-08 21:02:35 -0700

    one app test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,7 @@ describe('postgres-lab routes', () => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
 
-  it('create a new toy via POST', async() => {
+  it('create a new fish via POST', async() => {
     const response = await request(app)
       .post('/api/v1/fish')
       .send({ name: 'Salmon', color: 'pink', weight: '15' });
