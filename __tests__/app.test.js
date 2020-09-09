b03d6b5415d3fa878bece0167174fc74f6972a7a
internetcharles <charlesodellsmith@gmail.com>
2020-09-08 21:02:12 -0700

    one app test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,23 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fish = require('../lib/models/fish')
 
 describe('postgres-lab routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('create a new toy via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/fish')
+      .send({ name: 'Salmon', color: 'pink', weight: '15' });
+  
+  expect(response.body).toEqual({
+    id: expect.any(String),
+    name: 'Salmon',
+    color: 'pink',
+    weight: 15
+  });
+});
 });
