509624003a68fb2be57d9176f36401591c174123
internetcharles <charlesodellsmith@gmail.com>
2020-09-08 18:38:29 -0700

    sandwich model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS pizzas;
 DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS sandwiches;
+
 
 CREATE TABLE pizzas (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -13,4 +15,11 @@ CREATE TABLE games (
     title TEXT NOT NULL,
     genre TEXT NOT NULL,
     rating INT CHECK (rating > 0)
+);
+
+CREATE TABLE sandwiches (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    price INT CHECK (price > 0)
 );
